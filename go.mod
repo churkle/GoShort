ddac50357574adc39a0e44c6ce23f4bca5c051ee
churkle <charfei70@gmail.com>
2019-11-16 23:34:16 -0800

    finished database read/write

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module goshort
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/gorilla/mux v1.7.3
+	github.com/rafaeljusto/redigomock v0.0.0-20191016070255-2f957ca2c86c // indirect
+)
--- main.go
+++ main.go
@@ -1,35 +1,66 @@
 package main
 
 import (
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
+	"github.com/gomodule/redigo/redis"
 	"github.com/gorilla/mux"
 )
 
 var letterSet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var pool *redis.Pool
 
 func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+
+	url, err := getURL(vars["id"])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "GET called"}`))
+	io.WriteString(w, url)
 }
 
-func notFound(w http.ResponseWriter, r *http.Request) {
+func post(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+
+	id, err := shortenURL(vars["url"])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "NOT FOUND"}`))
+	io.WriteString(w, id)
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	pool = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+
+	setupRouter()
+}
+
+func setupRouter() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("/", get).Methods(http.MethodGet)
-	api.HandleFunc("/", notFound)
+	api.HandleFunc("/{id}", get).Methods(http.MethodGet)
+	api.HandleFunc("/shorten/{url}", post).Methods(http.MethodPost)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
@@ -40,4 +71,36 @@ func createIdentifier(length int) string {
 	}
 
 	return string(id)
+}
+
+func shortenURL(url string) (string, error) {
+	id := createIdentifier(5)
+	err := writeShortenedURLToDb(url, id)
+
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func writeShortenedURLToDb(url string, id string) error {
+	conn := pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("HMSET", id, "URL", url)
+
+	return err
+}
+
+func getURL(id string) (string, error) {
+	conn := pool.Get()
+	defer conn.Close()
+
+	url, err := redis.String(conn.Do("HGET", id, "URL"))
+	if err != nil {
+		return "", err
+	}
+
+	return url, err
 }
